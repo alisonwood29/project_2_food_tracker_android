5a8080d4a2f9b73249b362273b15b715da0db8da
Alison Wood <alisonwood29@gmail.com>
2018-01-29 10:35:19 +0000

    added a food tracker class with date and arraylist of dailyfood. 5 tests passing for getters

---
+++ app/src/main/java/example/alisonwood29/com/foodtrackerproject/FoodTracker.java
@@ -0,0 +1,49 @@
+package example.alisonwood29.com.foodtrackerproject;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * Created by alisonwood29 on 29/01/2018.
+ */
+
+public class FoodTracker {
+
+    private String date;
+    private ArrayList<DailyFood> dailyFoods;
+
+    public FoodTracker(String date){
+        this.date = date;
+        this.dailyFoods = new ArrayList<>();
+    }
+
+    public String getDate() {
+        return this.date;
+    }
+
+    public int dailyMealsCount() {
+        return this.dailyFoods.size();
+    }
+
+    public void addDailyFood(DailyFood dailyFood) {
+        this.dailyFoods.add(dailyFood);
+    }
+
+    public ArrayList<DailyFood> getDailyFoodsList() {
+        return this.dailyFoods;
+    }
+
+    public void formatDate() {
+        try{
+            Date formattedDate = new SimpleDateFormat("dd/MM/yyyy").parse(this.date);
+        } catch (ParseException e){
+            e.printStackTrace();
+        }
+    }
+
+//    public Date formatDate(String date) {
+//        Date formattedDate = new SimpleDateFormat("dd/MM/yyyy").parse(date);
+//    }
+}
---
+++ app/src/test/java/example/alisonwood29/com/foodtrackerproject/FoodTrackerTest.java
@@ -0,0 +1,62 @@
+package example.alisonwood29.com.foodtrackerproject;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by alisonwood29 on 29/01/2018.
+ */
+
+public class FoodTrackerTest {
+
+    FoodTracker foodTracker;
+    DailyFood dailyFood;
+    Breakfast breakfast;
+    Lunch lunch;
+    Dinner dinner;
+
+    @Before
+    public void before(){
+        foodTracker = new FoodTracker("29/01/2018");
+        breakfast = new Breakfast("toast with jam", 200);
+        lunch = new Lunch("chicken salad", 250);
+        dinner = new Dinner("pizza", 650);
+        dailyFood = new DailyFood(breakfast, lunch, dinner);
+    }
+
+    @Test
+    public void canGetDate(){
+        assertEquals("29/01/2018", foodTracker.getDate());
+    }
+
+    @Test
+    public void canFormatStringToDate(){
+        foodTracker.formatDate();
+        assertEquals("29/01/2018", foodTracker.getDate());
+    }
+
+    @Test
+    public void dailyMealsStartsEmpty(){
+        assertEquals(0, foodTracker.dailyMealsCount());
+    }
+
+    @Test
+    public void canAddToDailyMeals(){
+        foodTracker.addDailyFood(dailyFood);
+        assertEquals(1, foodTracker.dailyMealsCount());
+    }
+
+    @Test
+    public void canGetDailyMeals(){
+        foodTracker.addDailyFood(dailyFood);
+        ArrayList<DailyFood> expected = new ArrayList<>();
+        expected.add(dailyFood);
+        assertEquals(expected, foodTracker.getDailyFoodsList());
+
+    }
+}
