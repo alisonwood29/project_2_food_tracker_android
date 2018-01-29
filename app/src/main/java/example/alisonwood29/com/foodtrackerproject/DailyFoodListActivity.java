9f5ebffad0fc6d6a799cea57a70f056e5f3be3f9
Alison Wood <alisonwood29@gmail.com>
2018-01-29 14:56:17 +0000

    added a listview layout page

---
+++ app/src/main/java/example/alisonwood29/com/foodtrackerproject/DailyFoodListActivity.java
@@ -0,0 +1,13 @@
+package example.alisonwood29.com.foodtrackerproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DailyFoodListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_food_list);
+    }
+}
