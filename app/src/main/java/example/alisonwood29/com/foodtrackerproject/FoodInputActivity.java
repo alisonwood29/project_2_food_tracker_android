3e68d9e79c8a9ee043a131b7dafd48a3809f3f6b
Alison Wood <alisonwood29@gmail.com>
2018-01-29 11:58:42 +0000

    initialised edit text fields in food input activity

--- app/src/main/java/example/alisonwood29/com/foodtrackerproject/FoodInputActivity.java
+++ app/src/main/java/example/alisonwood29/com/foodtrackerproject/FoodInputActivity.java
@@ -2,12 +2,24 @@ package example.alisonwood29.com.foodtrackerproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class FoodInputActivity extends AppCompatActivity {
+
+    EditText date;
+    EditText breakfastFood;
+    EditText lunchFood;
+    EditText dinnerFood;
+    EditText breakfastCalories;
+    EditText lunchCalories;
+    EditText dinnerCalories;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_input);
+
+        date = findViewById(R.id.enter_date);
+
     }
 }
