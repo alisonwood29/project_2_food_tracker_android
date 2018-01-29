c1d37bf839273e2b0f33df8758c05e5fb5e20945
Alison Wood <alisonwood29@gmail.com>
2018-01-29 15:32:33 +0000

    added a menu on the food input page to go to the list view page

--- app/src/main/java/example/alisonwood29/com/foodtrackerproject/FoodInputActivity.java
+++ app/src/main/java/example/alisonwood29/com/foodtrackerproject/FoodInputActivity.java
@@ -1,8 +1,12 @@
 package example.alisonwood29.com.foodtrackerproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -34,6 +38,22 @@ public class FoodInputActivity extends AppCompatActivity {
         dinnerCalories = findViewById(R.id.dinner_calories);
         saveDailyFoods = findViewById(R.id.save_button);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.action_daily_food_list){
+            Intent intent = new Intent(this, DailyFoodListActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void onSaveButtonClick(View button){
